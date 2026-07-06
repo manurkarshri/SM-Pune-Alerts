9fb3045a0f37b3d4765b44ea258a2cb8122a5c91
manurkarshri <manurkar.shri@gmail.com>
2026-07-06 16:01:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,51 @@
-const CACHE_NAME = 'pune-alerts-v1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
+const CACHE_NAME = "emergency-command-hub-v2";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./alerts.json"
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.pathname.endsWith("/alerts.json")) {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
